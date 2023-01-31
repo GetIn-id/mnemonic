8a1ed299e58535483d3ba2b480d47728bfa4f5f1
theGrape1337 <thegrape1337@protonmail.com>
2023-01-31 21:14:56 +0100

    nostr setup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,14 @@
-#![cfg_attr(
+use tauri::Manager;
+
+#[cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  message: String,
+}
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -12,6 +19,19 @@ fn greet(name: &str) -> String {
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .setup(|app| {
+            // listen to the `event-name` (emitted on any window)
+            // let id = app.listen_global("event-name", |event| {
+            //   println!("got event-name with payload {:?}", event.payload());
+            // });
+            // unlisten to the event using the `id` returned on the `listen_global` function
+            // an `once_global` API is also exposed on the `App` struct
+            // app.unlisten(id);
+      
+            // emit the `event-name` event to all webview windows on the frontend
+            app.emit_all("event-name", Payload { message: "Tauri is awesome!".into() }).unwrap();
+            Ok(())
+          })
+          .run(tauri::generate_context!())
+          .expect("failed to run app");
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,97 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
+import { emit, listen } from '@tauri-apps/api/event'
 import "./App.css";
+import {relayInit} from 'nostr-tools';
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  // const [relay, setRelay] = useState(null);
+  // const [publicKey, setPublicKey] = useState(
+  //   '66a2eec5ef4a0c232c3c7f8720838a446296194742fe001ccb8dbb926b72518b',
+  // );
+  // const [allEvents, setAllEvents] = useState([]);
+  // const [metaData, setMetaData] = useState({
+  //   name: '',
+  //   display_name: 'Anon',
+  //   picture: '',
+  //   about: '',
+  // });
+
+  useEffect(() => {
+    //listen to a event
+    listen("event-name", (e) => {
+      console.log(e.payload);
+    });
+  }, [greetMsg]);
+
+  // useEffect(() => {
+  //   const connectRelays = async () => {
+  //     const relay = relayInit('wss://nostr.relayer.se');
+  //     await relay.connect();
+  //     relay.on('connect', () => {
+  //       setRelay(relay);
+  //       console.log(`connected to ${relay.url}`);
+  //     });
+  //     relay.on('error', () => {
+  //       console.log(`failed to connect to ${relay.url}`);
+  //     });
+  //   };
+  //   connectRelays();
+  // }, []);
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name }));
   }
 
+  // useEffect(() => {
+  //   if (relay !== null) {
+  //     let sub = relay.sub([
+  //       {
+  //         authors: [
+  //           publicKey,
+  //         ],
+  //         kinds: [0],
+  //       },
+  //     ]);
+  //     sub.on('event', event => {
+  //       if (allEvents.some(e => e.id === event.id)) {
+  //         /* event already exists */
+  //         console.log('event exist');
+  //       } else {
+  //         allEvents.push(event);
+  //         console.log(event);
+  //         const content = JSON.parse(event.content);
+  //         setMetaData(content);
+  //       }
+  //     });
+  //   } else {
+  //     console.log('no relay');
+  //   }
+  // }, [relay]);
+
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
+      <h1>Welcome to mnemonic!</h1>
+      Input your public key below to generate your username
       <div className="row">
         <div>
           <input
             id="greet-input"
             onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
+            placeholder="Enter your pubkey..."
           />
           <button type="button" onClick={() => greet()}>
-            Greet
+            Load name
           </button>
         </div>
       </div>
 
       <p>{greetMsg}</p>
+      {/* <p>{metaData.display_name}</p> */}
     </div>
   );
 }
