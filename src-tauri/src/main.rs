8c978aab17570f059cd638e8939c40edb5c792ad
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2023-02-01 20:44:32 +0100

    Add new dependencies, basic nostr client example

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,4 +1,11 @@
-use std::time::Duration;
+use dotenv::dotenv;
+use nostr_rust::{
+    events::extract_events_ws,
+    nostr_client::{self, Client},
+    req::ReqFilter,
+    Identity, Message,
+};
+use std::{str::FromStr, sync::Arc, sync::Mutex, thread, time::Duration};
 use tauri::Manager;
 use tokio::time::sleep;
 
@@ -18,37 +25,92 @@ struct Payload {
     post: String,
 }
 
+// TODO: remove allow lint
+#[allow(dead_code)]
+fn handle_message(relay_url: &String, message: &Message) -> Result<(), String> {
+    println!("Received message from {relay_url}: {:?}", message);
+
+    let events = extract_events_ws(message);
+    println!("Events: {:?}", events);
+
+    Ok(())
+}
+
 fn main() {
+    // Load .env file
+    dotenv().ok();
+
     tauri::Builder::default()
         .setup(|app| {
             let app_handle = app.app_handle();
+            // TODO: How should we really load nost private key
+            let private_key = std::env::var("NOSTR_PRIVATE_KEY").unwrap();
+            let public_key = std::env::var("NOSTR_PUBLIC_KEY").unwrap();
 
-            tauri::async_runtime::spawn(async move {
-                let post = "Post";
-                let mut i = 0;
-                loop {
-                    sleep(Duration::from_millis(2000)).await;
-                    println!(
-                        "{}",
-                        format_args!("Sending post from Rust backend: {post} {i}")
-                    );
-                    app_handle
-                        .emit_all(
-                            "feed-event",
-                            Payload {
-                                post: format!("{post} {i}"),
-                            },
-                        )
-                        .unwrap();
-                    println!("{}", i);
-                    i += 1;
+            let my_identity = Identity::from_str(&private_key).unwrap();
+
+            let nostr_client = Arc::new(Mutex::new(
+                Client::new(vec!["wss://nostr.relayer.se", "wss://relay.snort.social"]).unwrap(),
+            ));
+
+            // Run a new thread to handle messages
+            let nostr_clone = nostr_client.clone();
+            let handle_thread = thread::spawn(move || {
+                println!("Listening...");
+                let events = nostr_clone.lock().unwrap().next_data().unwrap();
+
+                for (relay_url, message) in events.iter() {
+                    handle_message(relay_url, message).unwrap();
                 }
             });
+
+            let subscription_id = nostr_client
+                .lock()
+                .unwrap()
+                .subscribe_with_id(
+                    "my_subscription_id",
+                    vec![ReqFilter {
+                        ids: None,
+                        authors: Some(vec![public_key.to_string()]),
+                        kinds: Some(vec![1]),
+                        e: None,
+                        p: None,
+                        since: None,
+                        until: None,
+                        limit: Some(1),
+                    }],
+                )
+                .unwrap();
+
+            // EXAMPLE, how to emit to frontend
+            //tauri::async_runtime::spawn(async move {
+            //    let post = "Post";
+            //    let mut i = 0;
+            //    loop {
+            //        sleep(Duration::from_millis(2000)).await;
+            //        println!(
+            //            "{}",
+            //            format_args!("Sending post from Rust backend: {post} {i}")
+            //        );
+            //        app_handle
+            //            .emit_all(
+            //                "feed-event",
+            //                Payload {
+            //                    post: format!("{post} {i}"),
+            //                },
+            //            )
+            //            .unwrap();
+            //        println!("{}", i);
+            //        i += 1;
+            //    }
+            //});
 
             app.listen_global("hello-from-frontend", |event| {
                 println!("got payload {:?}", event.payload());
             });
 
+            // Wait for the thread to finish
+            handle_thread.join().unwrap();
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![greet])
