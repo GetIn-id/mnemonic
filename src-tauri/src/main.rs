4378ae8e9e1be45e3ee5f7ce3cc638b65cce0754
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2023-02-01 18:51:04 +0100

    Add event listen and emit skeleton

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,37 +1,57 @@
+use std::time::Duration;
 use tauri::Manager;
+use tokio::time::sleep;
 
 #[cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-
-#[derive(Clone, serde::Serialize)]
-struct Payload {
-  message: String,
-}
-
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+// the payload type must implement `Serialize` and `Clone`.
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+    post: String,
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
         .setup(|app| {
-            // listen to the `event-name` (emitted on any window)
-            // let id = app.listen_global("event-name", |event| {
-            //   println!("got event-name with payload {:?}", event.payload());
-            // });
-            // unlisten to the event using the `id` returned on the `listen_global` function
-            // an `once_global` API is also exposed on the `App` struct
-            // app.unlisten(id);
-      
-            // emit the `event-name` event to all webview windows on the frontend
-            app.emit_all("event-name", Payload { message: "Tauri is awesome!".into() }).unwrap();
+            let app_handle = app.app_handle();
+
+            tauri::async_runtime::spawn(async move {
+                let post = "Post";
+                let mut i = 0;
+                loop {
+                    sleep(Duration::from_millis(2000)).await;
+                    println!(
+                        "{}",
+                        format_args!("Sending post from Rust backend: {post} {i}")
+                    );
+                    app_handle
+                        .emit_all(
+                            "feed-event",
+                            Payload {
+                                post: format!("{post} {i}"),
+                            },
+                        )
+                        .unwrap();
+                    println!("{}", i);
+                    i += 1;
+                }
+            });
+
+            app.listen_global("hello-from-frontend", |event| {
+                println!("got payload {:?}", event.payload());
+            });
+
             Ok(())
-          })
-          .run(tauri::generate_context!())
-          .expect("failed to run app");
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,75 @@
 import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
-import { emit, listen } from '@tauri-apps/api/event'
+import { listen, emit } from "@tauri-apps/api/event";
 import "./App.css";
-import {relayInit} from 'nostr-tools';
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
-  // const [relay, setRelay] = useState(null);
-  // const [publicKey, setPublicKey] = useState(
-  //   '66a2eec5ef4a0c232c3c7f8720838a446296194742fe001ccb8dbb926b72518b',
-  // );
-  // const [allEvents, setAllEvents] = useState([]);
-  // const [metaData, setMetaData] = useState({
-  //   name: '',
-  //   display_name: 'Anon',
-  //   picture: '',
-  //   about: '',
-  // });
+
+  const [speed, setFeed] = useState("");
 
   useEffect(() => {
-    //listen to a event
-    listen("event-name", (e) => {
-      console.log(e.payload);
+    const unlisten = listen("feed-event", (event) => {
+      console.log(event.payload.post)
+      setFeed(event.payload.post);
     });
-  }, [greetMsg]);
 
-  // useEffect(() => {
-  //   const connectRelays = async () => {
-  //     const relay = relayInit('wss://nostr.relayer.se');
-  //     await relay.connect();
-  //     relay.on('connect', () => {
-  //       setRelay(relay);
-  //       console.log(`connected to ${relay.url}`);
-  //     });
-  //     relay.on('error', () => {
-  //       console.log(`failed to connect to ${relay.url}`);
-  //     });
-  //   };
-  //   connectRelays();
-  // }, []);
+    return () => {
+      unlisten.then((f) => f());
+    };
+  }, []);
+
+
+  function sendEvent() {
+    emit("hello-from-frontend", {
+      theMessage: "Tauri is awesome!",
+    });
+  }
+
+
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name }));
   }
 
-  // useEffect(() => {
-  //   if (relay !== null) {
-  //     let sub = relay.sub([
-  //       {
-  //         authors: [
-  //           publicKey,
-  //         ],
-  //         kinds: [0],
-  //       },
-  //     ]);
-  //     sub.on('event', event => {
-  //       if (allEvents.some(e => e.id === event.id)) {
-  //         /* event already exists */
-  //         console.log('event exist');
-  //       } else {
-  //         allEvents.push(event);
-  //         console.log(event);
-  //         const content = JSON.parse(event.content);
-  //         setMetaData(content);
-  //       }
-  //     });
-  //   } else {
-  //     console.log('no relay');
-  //   }
-  // }, [relay]);
-
   return (
     <div className="container">
-      <h1>Welcome to mnemonic!</h1>
-      Input your public key below to generate your username
+      <h1>Welcome to Mnemonic!</h1>
+
+      <div className="row">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
       <div className="row">
         <div>
           <input
             id="greet-input"
             onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter your pubkey..."
+            placeholder="Enter a name..."
           />
           <button type="button" onClick={() => greet()}>
-            Load name
+            Greet
+          </button>
+          <button type="button" onClick={() => sendEvent()}>
+            SendEvent
           </button>
         </div>
       </div>
 
       <p>{greetMsg}</p>
-      {/* <p>{metaData.display_name}</p> */}
     </div>
   );
 }
