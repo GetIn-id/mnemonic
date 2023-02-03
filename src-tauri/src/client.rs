600194012dd325458f607145c545d15cd5183a56
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2023-02-04 00:52:14 +0100

    WIP, implementing ClientWrapper

--- src-tauri/src/client.rs
+++ src-tauri/src/client.rs
@@ -1,7 +1,13 @@
 use nostr_rust::{
     events::extract_events_ws, nostr_client::Client, req::ReqFilter, Identity, Message,
 };
-use tauri::AppHandle;
+use serde::{Deserialize, Serialize};
+use tauri::{AppHandle, Manager};
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Post {
+    content: String,
+}
 
 #[derive()]
 pub struct ClientWrapper {
@@ -12,11 +18,8 @@ pub struct ClientWrapper {
 
 impl ClientWrapper {
     pub fn new(identity: Identity, app_handle: AppHandle) -> Self {
-        let nostr_client = tauri::async_runtime::block_on(Client::new(vec![
-            "wss://nostr.relayer.se",
-            "wss://relay.snort.social",
-        ]))
-        .unwrap();
+        let nostr_client =
+            tauri::async_runtime::block_on(Client::new(vec!["wss://nos.lol"])).unwrap();
 
         Self {
             nostr_client,
@@ -34,33 +37,48 @@ impl ClientWrapper {
         println!("Received message from {}: {:?}", relay_url, message);
 
         let events = extract_events_ws(message);
-        println!("Events: {:?}", events);
+        println!("Events: {:?}\n", events);
+
+        for event in events {
+            if let Err(err) = self.app_handle.emit_all(
+                "feed-event",
+                Post {
+                    content: event.get_content(),
+                },
+            ) {
+                eprintln!("error when emitting feed-event {:?}", err)
+            }
+        }
 
         Ok(())
     }
 
     pub async fn read_messages(&mut self) {
-        let events = self.nostr_client.next_data().await.unwrap();
-
-        for (relay_url, message) in events.iter() {
-            self.handle_message(relay_url, message).unwrap();
+        while let Ok(events) = self.nostr_client.next_data().await {
+            for (relay_url, message) in events.iter() {
+                self.handle_message(relay_url, message).unwrap();
+            }
         }
     }
 
     pub async fn subscribe(&mut self) {
         self.nostr_client
-            .subscribe(vec![ReqFilter {
-                ids: None,
-                authors: Some(vec![
-                    "884704bd421721e292edbff42eb77547fe115c6ff9825b08fc366be4cd69e9f6".to_string(),
-                ]),
-                kinds: None,
-                e: None,
-                p: None,
-                since: None,
-                until: None,
-                limit: Some(1),
-            }])
+            .subscribe_with_id(
+                "dummy_id_for_now",
+                vec![ReqFilter {
+                    ids: None,
+                    authors: Some(vec![
+                        "884704bd421721e292edbff42eb77547fe115c6ff9825b08fc366be4cd69e9f6"
+                            .to_string(),
+                    ]),
+                    kinds: None,
+                    e: None,
+                    p: None,
+                    since: None,
+                    until: None,
+                    limit: Some(1),
+                }],
+            )
             .await
             .unwrap();
     }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,7 @@
 use dotenv::dotenv;
-use nostr_rust::{
-    events::extract_events_ws,
-    nostr_client::{self, Client},
-    req::ReqFilter,
-    Identity, Message,
-};
-use std::{str::FromStr, sync::Arc, sync::Mutex, thread, time::Duration};
-use tauri::{AppHandle, Manager};
-use tokio::time::sleep;
+use nostr_rust::Identity;
+use std::str::FromStr;
+use tauri::Manager;
 
 mod client;
 use client::ClientWrapper;
@@ -29,21 +23,6 @@ struct Payload {
     post: String,
 }
 
-// TODO: remove allow lint
-#[allow(dead_code)]
-fn handle_message(relay_url: &String, message: &Message) -> Result<String, String> {
-    //println!("Received message from {relay_url}: {:?}", message);
-
-    let events = extract_events_ws(message);
-    println!("Events: {:?}", events[0].get_content());
-
-    let content = events[0].get_content();
-
-    println!("\n");
-
-    Ok(content)
-}
-
 fn main() {
     // Load .env file
     dotenv().ok();
@@ -53,7 +32,7 @@ fn main() {
             let app_handle = app.app_handle();
             // TODO: How should we really load nost private key
             let private_key = std::env::var("NOSTR_PRIVATE_KEY").unwrap();
-            let public_key = std::env::var("NOSTR_PUBLIC_KEY").unwrap();
+            let _public_key = std::env::var("NOSTR_PUBLIC_KEY").unwrap();
 
             let my_identity = Identity::from_str(&private_key).unwrap();
 
